b03143dab79b59ef28e5b5437fe48b1a32e740e1
André Freitas <freitasandre38@gmail.com>
2023-08-18 09:36:23 -0300

    fix: create new card style fixed

--- frontend/src/components/card/index.js
+++ frontend/src/components/card/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import "./index.css"
+import "./index.css";
 
 export default function Card(props) {
     return (
-        <div className="card mx-2 shadow mb-5 bg-white rounded d-flex flex-column justify-content-between">
+        <div className="card mx-2 shadow mb-3">
             <div className='m-1'>
                 <h6>Título: {props.title}</h6>
                 <p>Cadastrado em: {props.date}</p>
             </div>
-            <button className="align-self-end btn bg-custom-purple text-custom-white">Detalhar</button>
+            <div className='d-flex justify-content-between'>
+                <button className="card-btn-danger align-self-start rounded m-2">Excluir</button>
+                <button className="card-btn align-self-end rounded m-2">Detalhar</button>
+            </div>
         </div>
     )
 }
