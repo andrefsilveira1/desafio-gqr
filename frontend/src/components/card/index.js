0d325d48c27c959ebce6a3282121941e14c74292
André Freitas <freitasandre38@gmail.com>
2023-08-20 15:08:59 -0300

    fix: remove bug in toast container

--- frontend/src/components/card/index.js
+++ frontend/src/components/card/index.js
@@ -12,9 +12,9 @@ export default function Card(props) {
         axios.delete(`http://localhost:3001/submissoes/${id}`)
             .then(response => {
                 console.log("RETURN:", response.data);
-                toast.success('Card removido com sucesso!');
                 props.fetchInitialCards();
                 handleCloseModal();
+                toast.success('Card removido com sucesso!', { toastId: 'success' });
             })
             .catch(error => {
                 console.log("Erro ao deletar o card", error);
@@ -31,34 +31,35 @@ export default function Card(props) {
     }
 
     return (
-        <div className="card mx-2 shadow mb-3 pointer">
-            <div className='m-1'>
-                <h6>Título: {props.title}</h6>
-                <small><p>Cadastrado em: {props.date}</p></small>
-            </div>
-            <div className='d-flex justify-content-between'>
-                <button className="btn btn-danger btn-sm align-self-start rounded m-2" onClick={handleShowModal}>Excluir</button>
-                <button className="btn custom-detalhar-btn btn-sm align-self-end rounded m-2">Detalhar</button>
-            </div>
-
-            <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Confirmar Exclusão</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Tem certeza de que deseja excluir o card com o título "{props.title}"?
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseModal}>
-                        Cancelar
-                    </Button>
-                    <Button variant="danger" onClick={() => deleteCard(props.cardId)}>
-                        Excluir
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-
+        <>
             <ToastContainer />
-        </div>
+            <div className="card mx-2 shadow mb-3 pointer">
+                <div className='m-1'>
+                    <h6>Título: {props.title}</h6>
+                    <small><p>Cadastrado em: {props.date}</p></small>
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <button className="btn btn-danger btn-sm align-self-start rounded m-2" onClick={handleShowModal}>Excluir</button>
+                    <button className="btn custom-detalhar-btn btn-sm align-self-end rounded m-2">Detalhar</button>
+                </div>
+
+                <Modal show={showModal} onHide={handleCloseModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Confirmar Exclusão</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Tem certeza de que deseja excluir o card com o título "{props.title}"?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleCloseModal}>
+                            Cancelar
+                        </Button>
+                        <Button variant="danger" onClick={() => deleteCard(props.cardId)}>
+                            Excluir
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        </>
     )
 }
